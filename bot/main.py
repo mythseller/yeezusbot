9fb12467e6da60b02c416cecc459edfd27e380b0
mythseller <77974882+mythseller@users.noreply.github.com>
2022-02-09 00:27:38 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -13,10 +13,16 @@ async def on_message(message):
     if message.author.bot: return
     guild = message.guild  
     target_channel = guild.get_channel(940719313638293544)  
-    target_role = guild.get_role(940612391819939840) 
+    role_1 = guild.get_role(940612391819939840)  
+    role_2 = guild.get_role(940731429426434069)
+    role_3 = guild.get_role(940731660440326154) 
     role_mentions = message.role_mentions
 
-    if target_role in role_mentions:
+    if role_1 in message.role_mentions: 
+        await target_channel.send(message.author.name + " -- " + message.content)
+    elif role_2 in message.role_mentions: 
+        await target_channel.send(message.author.name + " -- " + message.content)
+    elif role_3 in message.role_mentions: 
         await target_channel.send(message.author.name + " -- " + message.content)
 
 
