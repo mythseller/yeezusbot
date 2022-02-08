629f32c84deca92b2a6a6913d0e388bd83d522f7
mythseller <77974882+mythseller@users.noreply.github.com>
2022-02-09 00:12:58 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -4,9 +4,6 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix="!")
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-
-
-
 @bot.listen()
 async def on_ready():
     print(f"Logged in as {bot.user.name}({bot.user.id})")
@@ -17,6 +14,8 @@ async def on_message(message):
     guild = message.guild  
     target_channel = guild.get_channel(940719313638293544)  
     target_role = guild.get_role(940612391819939840) 
+    role_mentions = message.role_mentions
+
     if target_role in role_mentions:
         await target_channel.send(message.author.name + " -- " + message.content)
 
