7b0606f90f90bc8b50b12242f023bee637cb50b6
mythseller <77974882+mythseller@users.noreply.github.com>
2022-02-08 22:08:53 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -11,12 +11,13 @@ async def on_ready():
 @bot.listen()
 async def on_message(message):  
     guild = message.guild  
-    target_channel = guild.get_channel(920900596117155860----poop)  
+    target_channel = guild.get_channel(920900596117155860)  
     target_role = guild.get_role(940612391819939840) 
     role_mentions = message.role_mentions
 
     if target_role in role_mentions:
         await target_channel.send(message.content)
+    if (message.author.bot) return;
 
 @bot.command()
 async def ping(ctx):
