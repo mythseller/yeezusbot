e6036d55a00f3fdc41debb3aed60e40d6aea6bc7
mythseller <77974882+mythseller@users.noreply.github.com>
2022-02-09 14:20:09 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,7 @@
 import os
 from discord.ext import commands
-
+intents = discord.Intents().all()
+client = commands.Bot(command_prefix=',', intents=intents)
 bot = commands.Bot(command_prefix="!")
 TOKEN = os.getenv("DISCORD_TOKEN")
 
