421170cc6e4535d40c0799505611fa3152ed1858
mythseller <77974882+mythseller@users.noreply.github.com>
2022-02-09 14:40:31 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -17,14 +17,18 @@ async def on_message(message):
     role_1 = guild.get_role(940612391819939840)  
     role_2 = guild.get_role(940731429426434069)
     role_3 = guild.get_role(940731660440326154) 
+    print(role_3)
+    print(message.role_mentions)
 
-    if role_1 in message.role_mentions: 
+"""
+if role_1 in message.role_mentions: 
         await target_channel.send(message.author.name + " -- " + message.content)
     elif role_2 in message.role_mentions: 
         await target_channel.send(message.author.name + " -- " + message.content)
     elif role_3 in message.role_mentions: 
         embed = discord.Embed(title=message.content, color=0x00ff00)
         await target_channel.send(message.author.name + " -- " + message.content)
+"""
     
 @bot.command()
 async def ping(ctx):
