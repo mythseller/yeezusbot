2bb6336f92a5f1958ed6ce4f57625b1712efd948
mythseller <77974882+mythseller@users.noreply.github.com>
2022-02-12 16:02:39 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -36,6 +36,17 @@ async def on_message(message):
             )
             await target_channel.send(embed=embed)
             await message.channel.send(embed=embed)
+        elif role_2 in message.role_mentions:
+            msg = message.content.strip(f'<@&{role.id}>')
+            embed = discord.Embed(
+                title=msg, color=0x5b7e63, timestamp=datetime.now()
+            )
+            embed.add_field(name='Trade Type:', value=role.mention)
+            embed.set_author(
+                name=message.author.display_name, icon_url=message.author.avatar_url
+            )
+            await target_channel.send(embed=embed)
+            await message.channel.send(embed=embed)
 
 @bot.command()
 async def ping(ctx):
