e57d27faa1a51f82d87b3aef9dddf36f57fab3bf
mythseller <77974882+mythseller@users.noreply.github.com>
2022-02-12 15:48:15 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -24,11 +24,7 @@ async def on_message(message):
     role_3 = guild.get_role(940731660440326154)
 
     if role_1 in message.role_mentions:
-        msg = message.content.strip(f'<@&{role.id}>')
-        embed = discord.Embed(title=msg, color=0xff0000, timestamp=datetime.now())
-        embed.add_field(name='Trade Type:', value=role.mention)
-        embed.set_author(name=message.author.display_name, icon_url=message.author.avatar_url)
-        await target_channel.send(embed=embed)
+        await target_channel.send(message.author.name + " -- " + message.content)
         await message.channel.send(embed=embed)
     elif role_2 in message.role_mentions:
         msg = message.content.strip(f'<@&{role.id}>')
