7be90bc87f9abff67cf6ce2acbefefe5ab871cb6
mythseller <77974882+mythseller@users.noreply.github.com>
2022-02-12 16:32:20 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -31,14 +31,14 @@ async def on_message(message):
         embed.set_author(name=message.author.display_name, icon_url=message.author.avatar_url)
         await target_channel.send(embed=embed)
         await message.channel.send(embed=embed)
-    if role_2 in role_mentions:
+    elif role_2 in role_mentions:
         msg = message.content.strip(f'<@&{role.id}>')
         embed = discord.Embed(title=msg, color=0x00ff00, timestamp=datetime.now())
         embed.add_field(name='Trade Type:', value=role.mention)
         embed.set_author(name=message.author.display_name, icon_url=message.author.avatar_url)
         await target_channel.send(embed=embed)
         await message.channel.send(embed=embed)
-    if role_3 in role_mentions:
+    elif role_3 in role_mentions:
         msg = message.content.strip(f'<@&{role.id}>')
         embed = discord.Embed(title=msg, color=0x0000ff, timestamp=datetime.now())
         embed.add_field(name='Trade Type:', value=role.mention)
