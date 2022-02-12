a496f955a2bef0be91acb83a0667a0b6c7c783d9
mythseller <77974882+mythseller@users.noreply.github.com>
2022-02-12 15:55:58 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -22,32 +22,20 @@ async def on_message(message):
     role_1 = guild.get_role(940612391819939840)
     role_2 = guild.get_role(940731429426434069)
     role_3 = guild.get_role(940731660440326154)
+    roles = [role_1, role_2, role_3]
 
-    if role_1 in message.role_mentions:
-        await target_channel.send(message.author.name + " -- ")
-        await message.channel.send(embed=embed)
-    elif role_2 in message.role_mentions:
-        msg = message.content.strip(f'<@&{role.id}>')
-        embed = discord.Embed(
-            title=msg, color=0x00ff00, timestamp=datetime.now()
-        )
-        embed.add_field(name='Trade Type:', value=role.mention)
-        embed.set_author(
-            name=message.author.display_name, icon_url=message.author.avatar_url
-        )
-        await target_channel.send(embed=embed)
-        await message.channel.send(embed=embed)
-    elif role_3 in message.role_mentions:
-        msg = message.content.strip(f'<@&{role.id}>')
-        embed = discord.Embed(
-            title=msg, color=0x0000ff, timestamp=datetime.now()
-        )
-        embed.add_field(name='Trade Type:', value=role.mention)
-        embed.set_author(
-            name=message.author.display_name, icon_url=message.author.avatar_url
-        )
-        await target2_channel.send(embed=embed)
-        await message.channel.send(embed=embed)
+    for role in roles:
+        if role in message.role_mentions:
+            msg = message.content.strip(f'<@&{role.id}>')
+            embed = discord.Embed(
+                title=msg, color=0x5b7e63, timestamp=datetime.now()
+            )
+            embed.add_field(name='Trade Type:', value=role.mention)
+            embed.set_author(
+                name=message.author.display_name, icon_url=message.author.avatar_url
+            )
+            await target_channel.send(embed=embed)
+            await message.channel.send(embed=embed)
 
 @bot.command()
 async def ping(ctx):
