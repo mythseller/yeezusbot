c17b37af22d05aca87849432de886146a2fd8428
mythseller <77974882+mythseller@users.noreply.github.com>
2022-02-12 15:04:38 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -11,22 +11,21 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 async def on_ready():
     print(f"Logged in as {bot.user.name}({bot.user.id})")
 
-
-@bot.listen()
 async def on_message(message):
     if message.author.bot:
         return
     guild = message.guild
     target_channel = guild.get_channel(940719313638293544)
-    target2_channel = guild.get_channel(942055833720750081)
     role_1 = guild.get_role(940612391819939840)
     role_2 = guild.get_role(940731429426434069)
     role_3 = guild.get_role(940731660440326154)
+    roles = [role_1, role_2, role_3]
 
-    if role_1 in message.role_mentions:
+    for role in roles:
+        if role in message.role_mentions:
             msg = message.content.strip(f'<@&{role.id}>')
             embed = discord.Embed(
-                title=msg, color=ff0000, timestamp=datetime.now()
+                title=msg, color=0x5b7e63, timestamp=datetime.now()
             )
             embed.add_field(name='Trade Type:', value=role.mention)
             embed.set_author(
@@ -34,30 +33,7 @@ async def on_message(message):
             )
             await target_channel.send(embed=embed)
             await message.channel.send(embed=embed)
-    elif role_2 in message.role_mentions: 
-            msg = message.content.strip(f'<@&{role.id}>')
-            embed = discord.Embed(
-                title=msg, color=0000ff, timestamp=datetime.now()
-            )
-            embed.add_field(name='Trade Type:', value=role.mention)
-            embed.set_author(
-                name=message.author.display_name, icon_url=message.author.avatar_url
-            )
-            await target_channel.send(embed=embed)
-            await message.channel.send(embed=embed)
-    elif role_3 in message.role_mentions: 
-            msg = message.content.strip(f'<@&{role.id}>')
-            embed = discord.Embed(
-                title=msg, color=00ff00, timestamp=datetime.now()
-            )
-            embed.add_field(name='Trade Type:', value=role.mention)
-            embed.set_author(
-                name=message.author.display_name, icon_url=message.author.avatar_url
-            )
-            await target2_channel.send(embed=embed)
-            await message.channel.send(embed=embed)
-
-
+            
 @bot.command()
 async def ping(ctx):
     await ctx.send("pong")
