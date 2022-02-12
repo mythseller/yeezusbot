d0265b3879be5ac3d6b818395d551ff8774882b8
mythseller <77974882+mythseller@users.noreply.github.com>
2022-02-12 17:44:37 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -13,118 +13,54 @@ async def on_ready():
 
 
 @bot.listen()
-#SPY Channel
 async def on_message(message):
     if message.author.bot:
         return
     guild = message.guild
-    target1_channel = guild.get_channel(882606056184897536)
-    role_1 = guild.get_role(942052888333676634)
-    roles = [role_1]
+    # Fetch target channels
+    target1_channel = guild.get_channel(940719313638293544)
+    target2_channel = guild.get_channel(940719313638293544)
+    target3_channel = guild.get_channel(942055833720750081)
+    # Fetch target roles
+    role_1 = guild.get_role(940612391819939840)
+    role_2 = guild.get_role(940731429426434069)
+    role_3 = guild.get_role(940731660440326154)
 
-    for role in roles:
-        if role in message.role_mentions:
-            msg = message.content.strip(f'<@&{role.id}>')
-            embed = discord.Embed(title="Market Commentary", description=msg, color=0x0be60b, timestamp=datetime.now())
-            embed.add_field(name='Index:', value=role.mention)
-            embed.set_author(name=message.author.display_name, icon_url=message.author.avatar_url)
-            await target1_channel.send(embed=embed)
-            await message.channel.send(embed=embed)
-            
-@bot.listen()
-#intraday
-async def on_message(message):
-    if message.author.bot:
-        return
-    guild = message.guild
-    target2_channel = guild.get_channel(879466084116336660)
-    role_2 = guild.get_role(942051860167159819)
-    roles = [role_2]
+    if role_1 in message.role_mentions:
+        msg = message.content.strip(f"<@&{role_1.id}>")
+        embed = discord.Embed(
+            title="Market Commentary",
+            description=msg,
+            color=0x0BE60B,
+            timestamp=datetime.now(),
+        )
+        embed.add_field(name="Index:", value=role_1.mention)
+        embed.set_author(
+            name=message.author.display_name, icon_url=message.author.avatar_url
+        )
+        await target1_channel.send(embed=embed)
+        await message.channel.send(embed=embed)
 
-    for role in roles:
-        if role in message.role_mentions:
-            msg = message.content.strip(f'<@&{role.id}>')
-            embed = discord.Embed(title=msg, color=0x349434, timestamp=datetime.now())
-            embed.add_field(name='Trade Type:', value=role.mention)
-            embed.set_author(name=message.author.display_name, icon_url=message.author.avatar_url)
-            await target2_channel.send(embed=embed)
-            await message.channel.send(embed=embed)
+    elif role_2 in message.role_mentions:
+        msg = message.content.strip(f"<@&{role_2.id}>")
+        embed = discord.Embed(title=msg, color=0x0000FF, timestamp=datetime.now())
+        embed.add_field(name="Trade Type:", value=role_2.mention)
+        embed.set_author(
+            name=message.author.display_name, icon_url=message.author.avatar_url
+        )
+        await target2_channel.send(embed=embed)
+        await message.channel.send(embed=embed)
 
-@bot.listen()
-#scalp
-async def on_message(message):
-    if message.author.bot:
-        return
-    guild = message.guild
-    target3_channel = guild.get_channel(879466084116336660)
-    role_3 = guild.get_role(942052324615004250)
-    roles = [role_3]
+    elif role_3 in message.role_mentions:
+        msg = message.content.strip(f"<@&{role_3.id}>")
+        embed = discord.Embed(title=msg, color=0xFF0000, timestamp=datetime.now())
+        embed.add_field(name="Trade Type:", value=role_3.mention)
+        embed.set_author(
+            name=message.author.display_name, icon_url=message.author.avatar_url
+        )
+        await target3_channel.send(embed=embed)
+        await message.channel.send(embed=embed)
 
-    for role in roles:
-        if role in message.role_mentions:
-            msg = message.content.strip(f'<@&{role.id}>')
-            embed = discord.Embed(title=msg, color=0x782ea6, timestamp=datetime.now())
-            embed.add_field(name='Trade Type:', value=role.mention)
-            embed.set_author(name=message.author.display_name, icon_url=message.author.avatar_url)
-            await target3_channel.send(embed=embed)
-            await message.channel.send(embed=embed)
-            
-@bot.listen()
-#risky
-async def on_message(message):
-    if message.author.bot:
-        return
-    guild = message.guild
-    target4_channel = guild.get_channel(879466084116336660)
-    role_4 = guild.get_role(942052501228777493)
-    roles = [role_4]
-
-    for role in roles:
-        if role in message.role_mentions:
-            msg = message.content.strip(f'<@&{role.id}>')
-            embed = discord.Embed(title=msg, color=0xc45a25, timestamp=datetime.now())
-            embed.add_field(name='Trade Type:', value=role.mention)
-            embed.set_author(name=message.author.display_name, icon_url=message.author.avatar_url)
-            await target4_channel.send(embed=embed)
-            await message.channel.send(embed=embed)
-            
-@bot.listen()
-#lotto
-async def on_message(message):
-    if message.author.bot:
-        return
-    guild = message.guild
-    target5_channel = guild.get_channel(879466084116336660)
-    role_5 = guild.get_role(942052699921321984)
-    roles = [role_5]
-
-    for role in roles:
-        if role in message.role_mentions:
-            msg = message.content.strip(f'<@&{role.id}>')
-            embed = discord.Embed(title=msg, color=0xe31e87, timestamp=datetime.now())
-            embed.add_field(name='Trade Type:', value=role.mention)
-            embed.set_author(name=message.author.display_name, icon_url=message.author.avatar_url)
-            await target5_channel.send(embed=embed)
-            await message.channel.send(embed=embed)
-            
-@bot.listen()
-#futures
-async def on_message(message):
-    if message.author.bot:
-        return
-    guild = message.guild
-    target5_channel = guild.get_channel(879466084116336660)
-    role_5 = guild.get_role(942052789100617728)
-    roles = [role_5]
-
-    for role in roles:
-        if role in message.role_mentions:
-            msg = message.content.strip(f'<@&{role.id}>')
-            embed = discord.Embed(title=msg, color=0xe0dd12, timestamp=datetime.now())
-            embed.add_field(name='Trade Type:', value=role.mention)
-            embed.set_author(name=message.author.display_name, icon_url=message.author.avatar_url)
-            await target5_channel.send(embed=embed)
-            await message.channel.send(embed=embed)
 
 @bot.command()
 async def ping(ctx):
